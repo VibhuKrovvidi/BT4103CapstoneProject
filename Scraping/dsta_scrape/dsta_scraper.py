3f9996363948b8f34bbac794c828c52ccb750288
vibhukrovvidi <vibkrovvidi@gmail.com>
2021-03-21 23:27:27 +0800

    deleted sa

--- Scraping/dsta_scrape/dsta_scraper.py
+++ Scraping/dsta_scrape/dsta_scraper.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.remote.remote_connection import RemoteConnection
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import ElementClickInterceptedException
 
 # Import standard tools
 import time
@@ -86,14 +88,118 @@ class DSTA_Scraper:
 			task = datastore.Entity(key=task_key)
 			task['content'] = content[c]
 			self.client.put(task)
-			#break; # To be removed in deployment
+			break; # To be removed in deployment
 
 			print('Saved {}: {}'.format(task.key.name, task['content']))
 		print("Google Review for " + location + " have been scraped and stored");
 
 
-	def get_harwarezone(self):
-		pass;
+	def get_harwarezone(self, forum="nsknowledge"):
+		
+		post_title = []
+		post_username = []
+		post_message = []
+		reply_to = []
+
+		driver = webdriver.Chrome()
+		#for each page:
+		#load the forum
+		driver.get('https://forums.hardwarezone.com.sg/national-service-knowledge-base-162/saf-ippt-ipt-rt-questions-4220677-380.html')
+		time.sleep(3)
+
+		#title of forum
+		forum_title = driver.find_element_by_class_name('header-gray').text
+		print('forum title = ' + forum_title)
+
+		next_text = "Next ›"
+		flag = True
+		next_button = driver.find_element_by_class_name("prevnext")
+		buttons = driver.find_elements_by_class_name("prevnext")
+		for b in buttons:
+		    if b.text == next_text:
+		        next_button = b
+
+
+
+		while flag:
+		    #get list of all posts
+		    print('getting posts')
+		    posts = driver.find_elements_by_class_name('alt1')
+
+		    #drop last 2, then drop every alternate
+		    posts.pop()
+		    posts.pop()
+		    del posts[1::2]
+		    print('done getting posts')
+
+		    names = driver.find_elements_by_class_name('bigusername')
+		    for n in names:
+		        post_username.append(n.text)
+
+		    #message (whole box) = post_message, message being replied to = quote
+		    #for every post, check if there is quote.
+		    for p in posts:
+		        #get the full message, including prev if present
+		        full = p.find_element_by_class_name('post_message').text
+		        #check if there is reply
+		        try:
+		            reply = p.find_element_by_class_name('quote').text
+		            reply = reply.split('\n')
+		            full = full.split('\n')[1:]
+		            #wtv that is in the full message, but not in the quote is the reply for that user
+		            new = [i for i in full if i not in reply]
+		            new = " ".join(new) #join to one string
+		            reply = " ".join(reply)
+		            post_message.append(new) #adding the actual message to a list
+		            reply_to.append(reply) #adding the reply to a list if there is
+		            post_title.append(forum_title) #adding title
+		        except NoSuchElementException:
+		            full = full.split('\n')
+		            full = " ".join(full)
+		            post_message.append(full)
+		            reply_to.append('NIL') #no replies
+		            post_title.append(forum_title)
+
+		    if next_button.text == next_text:
+		        print('clicking next')
+		        next_button.click()
+		    else:
+		        flag = False
+
+		    buttons_list = []
+		    buttons = driver.find_elements_by_class_name("prevnext")
+		    for b in buttons:
+		        buttons_list.append(b.text)
+
+		    for b in buttons:
+		        if b.text == next_text:
+		            next_button = b
+		            
+		    if next_text not in buttons_list:
+		        next_button = driver.find_element_by_class_name("prevnext")
+		    
+		# data = pd.DataFrame(list(zip(post_title,post_username,post_message,reply_to)), columns = ['post_title','post_username', 'post', 'reply to'])
+		# data.to_csv("hwz_Post_title_output.csv")
+		driver.close()
+		
+		kind = "hardwarezone_posts"
+		for c in range(0, len(post_message)):
+			if post_message[c] == "":
+				continue;
+			else:
+				name = forum + "_replynumber_" + str(c+1)
+				task_key = self.client.key(kind, name)
+				task = datastore.Entity(key=task_key)
+				task['content'] = comments[c]
+				self.client.put(task)
+				print('Saved {}: {}'.format(task.key.name, task['content']))
+				break; # To be removed in deployment
+		
+			
+		print("Reddit Data for " + subreddit_name + " have been scraped and stored");
+
+
+
 
 	def submissions_pushshift_praw(self, subreddit, reddit, start=None, end=None, limit=100, extra_query=""):
 	    """
@@ -246,9 +352,9 @@ def main():
 	#scraper.get_google_reviews("https://www.google.com/maps/place/CMPB/@1.280195,103.815126,17z/data=!4m7!3m6!1s0x31da1bd0af54732f:0x9c274decbab4e599!8m2!3d1.280195!4d103.815126!9m1!1b1", "CMPB")
 
 	# Test harewarezone
-
+	scraper.get_harwarezone();
 	# Test reddit
-	scraper.get_reddit();
+	#scraper.get_reddit();
 	"""
 	# Initialise list of greview_urls
 	greview_urls = [] # Store a tupple. Eg: ("google.com", "CMPB")
