3299269334258ca31d0b149bc1adef96b47128cd
vibhukrovvidi <vibkrovvidi@gmail.com>
2021-03-22 19:58:24 +0800

    Made browser headless and updated dockerfile

--- code/dsta_scrape/dsta_scraper.py
+++ code/dsta_scrape/dsta_scraper.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.chrome.options import Options
 
 # Import standard tools
 import time
@@ -26,7 +27,14 @@ from google.cloud import datastore
 
 class DSTA_Scraper:
 	def __init__(self):
-		
+		self.chrome_options = Options();
+		self.chrome_options.add_argument("--headless")
+		self.chrome_options.add_argument("--no-sandbox")
+		self.chrome_options.add_argument("--disable-dev-shm-usage")
+		self.chrome_prefs = {}
+		self.chrome_options.experimental_options["prefs"] = self.chrome_prefs
+		self.chrome_prefs["profile.default_content_settings"] = {"images": 2}
+		#self.driver = webdriver.Chrome(options=chrome_options)
 
 		"""
 		webdriver.Remote(
@@ -47,7 +55,8 @@ class DSTA_Scraper:
 
 	def get_google_reviews(self, url, location = "CMPB"):
 
-		driver = webdriver.Chrome()
+		#driver = webdriver.Chrome()
+		driver = webdriver.Chrome(options=self.chrome_options)
 		driver.get(url);
 
 		delay = 3# seconds
@@ -109,7 +118,9 @@ class DSTA_Scraper:
 		post_message = []
 		reply_to = []
 
-		driver = webdriver.Chrome()
+		# driver = webdriver.Chrome()
+		driver = webdriver.Chrome(options=self.chrome_options)
+
 		#for each page:
 		#load the forum
 		driver.get('https://forums.hardwarezone.com.sg/national-service-knowledge-base-162/saf-ippt-ipt-rt-questions-4220677-380.html')
@@ -124,68 +135,68 @@ class DSTA_Scraper:
 		next_button = driver.find_element_by_class_name("prevnext")
 		buttons = driver.find_elements_by_class_name("prevnext")
 		for b in buttons:
-		    if b.text == next_text:
-		        next_button = b
+			if b.text == next_text:
+				next_button = b
 
 
 
 		while flag:
-		    #get list of all posts
-		    print('getting posts')
-		    posts = driver.find_elements_by_class_name('alt1')
+			#get list of all posts
+			print('getting posts')
+			posts = driver.find_elements_by_class_name('alt1')
 
-		    #drop last 2, then drop every alternate
-		    posts.pop()
-		    posts.pop()
-		    del posts[1::2]
-		    print('done getting posts')
+			#drop last 2, then drop every alternate
+			posts.pop()
+			posts.pop()
+			del posts[1::2]
+			print('done getting posts')
 
-		    names = driver.find_elements_by_class_name('bigusername')
-		    for n in names:
-		        post_username.append(n.text)
+			names = driver.find_elements_by_class_name('bigusername')
+			for n in names:
+				post_username.append(n.text)
 
-		    #message (whole box) = post_message, message being replied to = quote
-		    #for every post, check if there is quote.
-		    for p in posts:
-		        #get the full message, including prev if present
-		        full = p.find_element_by_class_name('post_message').text
-		        #check if there is reply
-		        try:
-		            reply = p.find_element_by_class_name('quote').text
-		            reply = reply.split('\n')
-		            full = full.split('\n')[1:]
-		            #wtv that is in the full message, but not in the quote is the reply for that user
-		            new = [i for i in full if i not in reply]
-		            new = " ".join(new) #join to one string
-		            reply = " ".join(reply)
-		            post_message.append(new) #adding the actual message to a list
-		            reply_to.append(reply) #adding the reply to a list if there is
-		            post_title.append(forum_title) #adding title
-		        except NoSuchElementException:
-		            full = full.split('\n')
-		            full = " ".join(full)
-		            post_message.append(full)
-		            reply_to.append('NIL') #no replies
-		            post_title.append(forum_title)
+			#message (whole box) = post_message, message being replied to = quote
+			#for every post, check if there is quote.
+			for p in posts:
+				#get the full message, including prev if present
+				full = p.find_element_by_class_name('post_message').text
+				#check if there is reply
+				try:
+					reply = p.find_element_by_class_name('quote').text
+					reply = reply.split('\n')
+					full = full.split('\n')[1:]
+					#wtv that is in the full message, but not in the quote is the reply for that user
+					new = [i for i in full if i not in reply]
+					new = " ".join(new) #join to one string
+					reply = " ".join(reply)
+					post_message.append(new) #adding the actual message to a list
+					reply_to.append(reply) #adding the reply to a list if there is
+					post_title.append(forum_title) #adding title
+				except NoSuchElementException:
+					full = full.split('\n')
+					full = " ".join(full)
+					post_message.append(full)
+					reply_to.append('NIL') #no replies
+					post_title.append(forum_title)
 
-		    if next_button.text == next_text:
-		        print('clicking next')
-		        next_button.click()
-		    else:
-		        flag = False
+			if next_button.text == next_text:
+				print('clicking next')
+				next_button.click()
+			else:
+				flag = False
 
-		    buttons_list = []
-		    buttons = driver.find_elements_by_class_name("prevnext")
-		    for b in buttons:
-		        buttons_list.append(b.text)
+			buttons_list = []
+			buttons = driver.find_elements_by_class_name("prevnext")
+			for b in buttons:
+				buttons_list.append(b.text)
 
-		    for b in buttons:
-		        if b.text == next_text:
-		            next_button = b
-		            
-		    if next_text not in buttons_list:
-		        next_button = driver.find_element_by_class_name("prevnext")
-		    
+			for b in buttons:
+				if b.text == next_text:
+					next_button = b
+					
+			if next_text not in buttons_list:
+				next_button = driver.find_element_by_class_name("prevnext")
+			
 		# data = pd.DataFrame(list(zip(post_title,post_username,post_message,reply_to)), columns = ['post_title','post_username', 'post', 'reply to'])
 		# data.to_csv("hwz_Post_title_output.csv")
 		driver.close()
@@ -214,60 +225,60 @@ class DSTA_Scraper:
 
 
 	def submissions_pushshift_praw(self, subreddit, reddit, start=None, end=None, limit=100, extra_query=""):
-	    """
-	    A simple function that returns a list of PRAW submission objects during a particular period from a defined sub.
-	    This function serves as a replacement for the now deprecated PRAW `submissions()` method.
-	    
-	    :param subreddit: A subreddit name to fetch submissions from.
-	    :param start: A Unix time integer. Posts fetched will be AFTER this time. (default: None)
-	    :param end: A Unix time integer. Posts fetched will be BEFORE this time. (default: None)
-	    :param limit: There needs to be a defined limit of results (default: 100), or Pushshift will return only 25.
-	    :param extra_query: A query string is optional. If an extra_query string is not supplied, 
-	                        the function will just grab everything from the defined time period. (default: empty string)
-	    
-	    Submissions are yielded newest first.
-	    
-	    For more information on PRAW, see: https://github.com/praw-dev/praw 
-	    For more information on Pushshift, see: https://github.com/pushshift/api
-	    """
-	    matching_praw_submissions = []
-	    
-	    # Default time values if none are defined (credit to u/bboe's PRAW `submissions()` for this section)
-	    utc_offset = 28800
-	    now = int(time.time())
-	    start = max(int(start) + utc_offset if start else 0, 0)
-	    end = min(int(end) if end else now, now) + utc_offset
-	    
-	    # Format our search link properly.
-	    search_link = ('https://api.pushshift.io/reddit/submission/search/'
-	                   '?subreddit={}&after={}&before={}&sort_type=score&sort=asc&limit={}&q={}')
-	    search_link = search_link.format(subreddit, start, end, limit, extra_query)
-	    
-	    # Get the data from Pushshift as JSON.
-	    retrieved_data = requests.get(search_link)
-	    returned_submissions = retrieved_data.json()['data']
-	    
-	    i = 0
-	    # Iterate over the returned submissions to convert them to PRAW submission objects.
-	    for submission in returned_submissions:
-	        
-	        self.progress(i, len(returned_submissions), status='Collecting posts')
-	        # Take the ID, fetch the PRAW submission object, and append to our list
-	        praw_submission = reddit.submission(id=submission['id'])
-	        matching_praw_submissions.append(praw_submission)
-	        i += 1
-	     
-	    # Return all PRAW submissions that were obtained.
-	    return matching_praw_submissions
+		"""
+		A simple function that returns a list of PRAW submission objects during a particular period from a defined sub.
+		This function serves as a replacement for the now deprecated PRAW `submissions()` method.
+		
+		:param subreddit: A subreddit name to fetch submissions from.
+		:param start: A Unix time integer. Posts fetched will be AFTER this time. (default: None)
+		:param end: A Unix time integer. Posts fetched will be BEFORE this time. (default: None)
+		:param limit: There needs to be a defined limit of results (default: 100), or Pushshift will return only 25.
+		:param extra_query: A query string is optional. If an extra_query string is not supplied, 
+							the function will just grab everything from the defined time period. (default: empty string)
+		
+		Submissions are yielded newest first.
+		
+		For more information on PRAW, see: https://github.com/praw-dev/praw 
+		For more information on Pushshift, see: https://github.com/pushshift/api
+		"""
+		matching_praw_submissions = []
+		
+		# Default time values if none are defined (credit to u/bboe's PRAW `submissions()` for this section)
+		utc_offset = 28800
+		now = int(time.time())
+		start = max(int(start) + utc_offset if start else 0, 0)
+		end = min(int(end) if end else now, now) + utc_offset
+		
+		# Format our search link properly.
+		search_link = ('https://api.pushshift.io/reddit/submission/search/'
+					   '?subreddit={}&after={}&before={}&sort_type=score&sort=asc&limit={}&q={}')
+		search_link = search_link.format(subreddit, start, end, limit, extra_query)
+		
+		# Get the data from Pushshift as JSON.
+		retrieved_data = requests.get(search_link)
+		returned_submissions = retrieved_data.json()['data']
+		
+		i = 0
+		# Iterate over the returned submissions to convert them to PRAW submission objects.
+		for submission in returned_submissions:
+			
+			self.progress(i, len(returned_submissions), status='Collecting posts')
+			# Take the ID, fetch the PRAW submission object, and append to our list
+			praw_submission = reddit.submission(id=submission['id'])
+			matching_praw_submissions.append(praw_submission)
+			i += 1
+		 
+		# Return all PRAW submissions that were obtained.
+		return matching_praw_submissions
 
 	# Progress bar
 	def progress(self, count, total, status=''):
-	    bar_len = 60
-	    filled_len = int(round(bar_len * count / float(total)))
-	    percents = round(100.0 * count / float(total), 1)
-	    bar = '=' * filled_len + '-' * (bar_len - filled_len)
-	    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
-	    sys.stdout.flush()  # As suggested by Rom Ruben (see: http://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console/27871113#comment50529068_27871113)
+		bar_len = 60
+		filled_len = int(round(bar_len * count / float(total)))
+		percents = round(100.0 * count / float(total), 1)
+		bar = '=' * filled_len + '-' * (bar_len - filled_len)
+		sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+		sys.stdout.flush()  # As suggested by Rom Ruben (see: http://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console/27871113#comment50529068_27871113)
 
 
 	def get_reddit(self, subreddit_name = "NationalServiceSG"):
@@ -278,10 +289,10 @@ class DSTA_Scraper:
 
 		posts = []
 		for p in extracted_posts:
-		    try:
-		        posts.append([p.title, p.score, p.id, p.subreddit, p.url, p.num_comments, p.selftext, p.link_flair_template_id, p.link_flair_text, p.created])
-		    except AttributeError:
-		        posts.append([p.title, p.score, p.id, p.subreddit, p.url, p.num_comments, p.selftext, "None", "None", p.created])
+			try:
+				posts.append([p.title, p.score, p.id, p.subreddit, p.url, p.num_comments, p.selftext, p.link_flair_template_id, p.link_flair_text, p.created])
+			except AttributeError:
+				posts.append([p.title, p.score, p.id, p.subreddit, p.url, p.num_comments, p.selftext, "None", "None", p.created])
 
 		posts = pd.DataFrame(posts, columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'flair_id', 'flair', 'created'])
 		print("Posts:")
@@ -291,12 +302,12 @@ class DSTA_Scraper:
 		all_comments = []
 		i = 0
 		for iden in posts['id']:
-		    self.progress(i, len(posts.index), status='Collecting comments')
-		    post = reddit.submission(id=iden)
-		    post.comments.replace_more(limit=0)
-		    for c in post.comments.list():
-		        all_comments.append([c.score, c.id, c.subreddit, c.depth, c.body, c.created, iden])
-		    i += 1
+			self.progress(i, len(posts.index), status='Collecting comments')
+			post = reddit.submission(id=iden)
+			post.comments.replace_more(limit=0)
+			for c in post.comments.list():
+				all_comments.append([c.score, c.id, c.subreddit, c.depth, c.body, c.created, iden])
+			i += 1
 
 		all_comments = pd.DataFrame(all_comments, columns=['score', 'id', 'subreddit', 'depth', 'body', 'created', 'op_id'])
 		print("Comments:")
@@ -362,7 +373,7 @@ def main():
 
 
 	# Test greview
-	#scraper.get_google_reviews("https://www.google.com/maps/place/CMPB/@1.280195,103.815126,17z/data=!4m7!3m6!1s0x31da1bd0af54732f:0x9c274decbab4e599!8m2!3d1.280195!4d103.815126!9m1!1b1", "CMPB")
+	scraper.get_google_reviews("https://www.google.com/maps/place/CMPB/@1.280195,103.815126,17z/data=!4m7!3m6!1s0x31da1bd0af54732f:0x9c274decbab4e599!8m2!3d1.280195!4d103.815126!9m1!1b1", "CMPB")
 
 	# Test harewarezone
 	# scraper.get_harwarezone();
